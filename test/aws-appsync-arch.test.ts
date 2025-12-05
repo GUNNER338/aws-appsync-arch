42dd2d8ba2324d4e4c007c02fbd60981bc7ed093
Gurpartap Singh <gsranu338@gmail.com>
2025-12-05 22:36:28 +0530

    First Commit

---
+++ test/aws-appsync-arch.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsAppsyncArch from '../lib/aws-appsync-arch-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-appsync-arch-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsAppsyncArch.AwsAppsyncArchStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
